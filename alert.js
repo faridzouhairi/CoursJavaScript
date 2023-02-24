abd1fcafe0f95564de81d4c9fd59182e548f1d2c
zouhairi <zouhairi.farid@gmail.com>
2023-02-23 19:16:22 -0800

    first commit

---
+++ alert.js
@@ -0,0 +1,7 @@
+console.log("Bonjour de javascript");
+console.warn("Bonsoir de javascript");
+console.warn("Aurevoir de javascript");
+console.log("A bientot de javascript");
+document.getElementById("heading").innerHTML = "Une simple page HTML";
+document.getElementsByClassName('titre').innerHTML = Date();
+   
